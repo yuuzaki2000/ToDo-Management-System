840dae175a910ad90ed8b39e30fff60a9484070d
sinto <sinto@host.docker.internal>
2022-09-25 22:32:43 +0900

    修正

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -2,6 +2,7 @@ package com.dmm.task;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,7 @@ public class MainController {
 	private TasksRepository tasksRepository;
 
 	@GetMapping("/main")
-	public String getMain(Model model) {
+	public String Main(@AuthenticationPrincipal AccountUserDetails user,Model model) {
 		//カレンダーに年と月を記入
 		LocalDate nowDate = LocalDate.now();
 		int thisYear = nowDate.getYear();
@@ -36,106 +37,136 @@ public class MainController {
 		model.addAttribute("month", month);
 
 		//カレンダーを表示するための処理を記載
-List<List<LocalDate>> matrix = new ArrayList<>();
-		
-		//その月の1日（ついたち）のLocalDataを取得する
+		List<List<LocalDate>> matrix = new ArrayList<>();
+
+		//その月の1日（ついたち）のLocalDateを取得する
 		LocalDate firstOfMonth = LocalDate.now().withDayOfMonth(1);
-		
+
+		//その月の最後の日のLocalDateを取得する
+		LocalDate lastDayOfMonth = firstOfMonth.plusDays(LocalDate.now().lengthOfMonth());
+
 		//firstOfMonthの曜日をあらわすDayOfWeekを取得する
 		int dowFOM = firstOfMonth.getDayOfWeek().getValue();
-		
-		//その月の1日（ついたち）の週の日曜日にあたる日のLocalDateを取得する
+
+		//その月の1日（ついたち）の週の最初の日（日曜日）のLocalDateを取得する
 		LocalDate fisrtDayOfFirstWeek = firstOfMonth.minusDays(dowFOM);
-		
+
+		//lastDayOfMonthの曜日をあらわすDayOfWeekを取得する
+		int dowLDOM = lastDayOfMonth.getDayOfWeek().getValue();
+
+		//その月の末日の週の最後の日(土曜日）のLocalDateを取得する
+		LocalDate lastDayOfLastWeek = lastDayOfMonth.plusDays(6 - dowLDOM);
+
 		//注目する日付のLocalDateを作成
 		LocalDate date;
 		
+
+
 		//1週間目
-		date = fisrtDayOfFirstWeek;
+		date = fisrtDayOfFirstWeek;		
+		
 		List<LocalDate> week1 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week1.add(date);
-			
-			if(week1.size()==7) {
+
+			if (week1.size() == 7) {
 				matrix.add(week1);
 				break;
 			}
 		}
 		//2週間目
-		date = fisrtDayOfFirstWeek.plusDays(7);		
+		date = fisrtDayOfFirstWeek.plusDays(7);
 		List<LocalDate> week2 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week2.add(date);
-			
-			if(week2.size()==7) {
+
+			if (week2.size() == 7) {
 				matrix.add(week2);
 				break;
 			}
 		}
 		//3週間目
-		date = fisrtDayOfFirstWeek.plusDays(14);		
+		date = fisrtDayOfFirstWeek.plusDays(14);
 		List<LocalDate> week3 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week3.add(date);
-			
-			if(week3.size()==7) {
+
+			if (week3.size() == 7) {
 				matrix.add(week3);
 				break;
 			}
 		}
 		//4週間目
-		date = fisrtDayOfFirstWeek.plusDays(21);		
+		date = fisrtDayOfFirstWeek.plusDays(21);
 		List<LocalDate> week4 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week4.add(date);
-			
-			if(week4.size()==7) {
+
+			if (week4.size() == 7) {
 				matrix.add(week4);
 				break;
 			}
 		}
 		//5週間目
-		date = fisrtDayOfFirstWeek.plusDays(28);		
+		date = fisrtDayOfFirstWeek.plusDays(28);
 		List<LocalDate> week5 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week5.add(date);
-			
-			if(week5.size()==7) {
+
+			if (week5.size() == 7) {
 				matrix.add(week5);
 				break;
 			}
 		}
+		
 		//6週間目
-		date = fisrtDayOfFirstWeek.plusDays(35);		
+		date = fisrtDayOfFirstWeek.plusDays(35);
 		List<LocalDate> week6 = new ArrayList<LocalDate>();
-		while(true) {
+		while (true) {
+			if(lastDayOfMonth.compareTo(lastDayOfLastWeek)<=0) {
+				break;
+			}
 			LocalDate afterDay = date.plusDays(1);
 			date = afterDay;
 			week6.add(date);
-			
-			if(week6.size()==7) {
+
+			if (week6.size() == 7) {
 				matrix.add(week6);
 				break;
 			}
 		}
+		
+		//map作成
+		Map<LocalDate, List<Tasks>> map = new TreeMap<>();
+				
+		
+		LocalDateTime dateMap;
+		
+		LocalDateTime LDTfirstDayOfFirstWeek = fisrtDayOfFirstWeek.atStartOfDay();
+		
+		dateMap = LDTfirstDayOfFirstWeek;
+		while(true) {
+			LocalDateTime afterDay = dateMap.plusDays(1);
+			dateMap = afterDay;
+			LocalDate d =dateMap.toLocalDate();
+			map.put(d, tasksRepository.findByDateBetween(dateMap,dateMap.plusDays(1),user.getName()));
+			if(date.compareTo(lastDayOfLastWeek)<=0) {
+				break;
+			}
+		} 
 
-		
-		System.out.println(matrix);
 		model.addAttribute("matrix", matrix);
-		
-		Map<LocalDate,List<Tasks>> map= new TreeMap<>();
-		
-		
+
 		model.addAttribute("tasks", map);
 
 		return "main";
@@ -143,32 +174,38 @@ List<List<LocalDate>> matrix = new ArrayList<>();
 	}
 
 	@GetMapping("/main/create/{date}")
-	public String create() {
+	public String create(@PathVariable String date, Model model) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate d = LocalDate.parse(date, dtf);
+		model.addAttribute("date", d);
+
 		return "create";
 
 	}
-	
+
 	@GetMapping("/main/create")
 	public String setForm(Model model) {
 		TasksForm tasksForm = new TasksForm();
 		model.addAttribute("tasksForm", tasksForm);
 		return "create";
-		
+
 	}
-	
+
 	@PostMapping("/main/create")
-	public String register(TasksForm tasksForm,@AuthenticationPrincipal AccountUserDetails user) {
+	public String register(TasksForm tasksForm, @AuthenticationPrincipal AccountUserDetails user) {
 		Tasks tasksDatabase = new Tasks();
-		tasksDatabase.setTitle(tasksForm.getText());
+		tasksDatabase.setTitle(tasksForm.getTitle());
 		tasksDatabase.setName(user.getName());
 		tasksDatabase.setText(tasksForm.getText());
-		tasksDatabase.setDate(LocalDateTime.now());
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate dateData = LocalDate.parse(tasksForm.getDate(), dtf);
+		tasksDatabase.setDate(dateData);
 		tasksDatabase.setDone(false);
-		
+
 		tasksRepository.save(tasksDatabase);
-		
+
 		return "redirect:/main";
-		
+
 	}
 
 	@GetMapping("/main/edit/{id}")
@@ -176,12 +213,12 @@ List<List<LocalDate>> matrix = new ArrayList<>();
 		return "edit";
 
 	}
-	
+
 	@PostMapping("/main/delete/{id}")
 	public String delete(@PathVariable Integer id) {
 		tasksRepository.deleteById(id);
 		return "redirect:/edit";
-		
+
 	}
 
 }
--- src/main/java/com/dmm/task/form/TasksForm.java
+++ src/main/java/com/dmm/task/form/TasksForm.java
@@ -8,5 +8,7 @@ public class TasksForm {
 	
 	private String text;
 	
+	private String date;
+	
 
 }
